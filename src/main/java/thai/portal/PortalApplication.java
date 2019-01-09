05b1f9255b80b89e733a94b8bc77b59075f6a40e
Nguyen Nam Thai <namthainguyen@gmail.com>
2019-01-09 12:53:19 +0700

    Add Eureka

--- src/main/java/thai/portal/PortalApplication.java
+++ src/main/java/thai/portal/PortalApplication.java
@@ -2,10 +2,8 @@ package thai.portal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class PortalApplication {
     public static void main(String[] args) {
         SpringApplication.run(PortalApplication.class, args);
