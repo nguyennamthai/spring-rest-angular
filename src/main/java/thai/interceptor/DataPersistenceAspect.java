b6c9a47467f39e71f3e8c80df67b2bd629db5b5c
Nguyen Nam Thai <namthainguyen@gmail.com>
2019-01-09 10:57:53 +0700

    Remove security

--- src/main/java/thai/interceptor/DataPersistenceAspect.java
+++ src/main/java/thai/interceptor/DataPersistenceAspect.java
@@ -4,8 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,9 +14,9 @@ import org.springframework.stereotype.Component;
 public class DataPersistenceAspect {
     @AfterReturning("execution(public * org.springframework.data.repository.Repository+.save(*))")
     public void afterSaving(JoinPoint joinPoint) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication == null ? "anonymous" : authentication.getName();
-        Object argument = joinPoint.getArgs()[0];
-        log.info("The user " + username + " saved record " + argument);
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String username = authentication == null ? "anonymous" : authentication.getName();
+//        Object argument = joinPoint.getArgs()[0];
+//        log.info("The user " + username + " saved record " + argument);
     }
 }
